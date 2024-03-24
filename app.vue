348d6173d05978ab37122621ad10d4f87de52f81
kanelloc <babis.kane@gmail.com>
2024-03-24 02:29:11 +0200

    feat: set head tags

--- app.vue
+++ app.vue
@@ -5,3 +5,10 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Vue.js Directory',
+    meta: [{ name: 'Vue.js Directory', content: 'Vue.js libraries directory' }],
+  });
+</script>
