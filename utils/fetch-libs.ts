e9ae2555fbc6eaba674f30fb82500a027d914639
kanelloc <babis.kane@gmail.com>
2024-03-25 18:40:11 +0200

    feat: rename libraries data

--- utils/fetch-libs.ts
+++ utils/fetch-libs.ts
@@ -1,5 +1,5 @@
 import type { FormattedLibrary } from '~/types/libraries';
-import data from '../data/vuejs-libraries-cached.json';
+import data from '../data/data.json';
 
 export const fetchLibs = (): FormattedLibrary[] => {
   return data as FormattedLibrary[];
